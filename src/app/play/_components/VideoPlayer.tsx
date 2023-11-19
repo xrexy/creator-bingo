7259c36cc84483d384fe7d9d082f489f32442b93
xrexy <rexydoesimportantthings@gmail.com>
2023-11-19 14:06:15 +0200

    i HATE youtube iframe api

--- src/app/play/_components/VideoPlayer.tsx
+++ src/app/play/_components/VideoPlayer.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
+import YouTube, { YouTubeEvent, type YouTubeProps } from "react-youtube";
 import { BoardInfo } from "../shared";
-import YouTube, { YouTubeProps } from "react-youtube";
-import { on } from "events";
 
 export type VideoPlayerProps = {
   board: BoardInfo;
@@ -17,6 +16,15 @@ const stateNames = {
   "5": "cued",
 } as const;
 
+const opts: YouTubeProps["opts"] = {
+  height: "390",
+  width: "640",
+  playerVars: {
+    // https://developers.google.com/youtube/player_parameters
+    autoplay: 1,
+  },
+};
+
 const normalizeState = (state: number) =>
   stateNames[state as any as keyof typeof stateNames];
 
@@ -24,42 +32,56 @@ export function VideoPlayer({ board }: VideoPlayerProps) {
   const containerRef = useRef<HTMLIFrameElement>(null);
 
   const [currentTime, setCurrentTime] = useState(0);
+  const [shouldUpdateTime, setShouldUpdateTime] = useState(false);
+  const interval = useRef<NodeJS.Timeout | null>(null);
 
-  let interval: NodeJS.Timeout | null = null;
+  const destroyTimeUpdater = () => {
+    if (!interval.current) return;
+    console.log("destroy");
+    clearInterval(interval.current);
+    interval.current = null;
+  };
 
-  const onPlayerReady: YouTubeProps["onReady"] = (event) => {
-    if (interval) return;
-
-    interval = setInterval(async () => {
-      const time = await event.target.getCurrentTime();
+  const createTimeUpdater = async (e: YouTubeEvent) => {
+    if (interval.current) return;
+    console.log("create");
+    interval.current = setInterval(async () => {
+      const time = await e.target.getCurrentTime();
+      console.log("time", time);
       setCurrentTime(time);
     }, 1000);
   };
 
-  const opts: YouTubeProps["opts"] = {
-    height: "390",
-    width: "640",
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
-    },
-  };
+  useEffect(() => {
+    if (!shouldUpdateTime) destroyTimeUpdater();
+  }, [shouldUpdateTime]);
 
   useEffect(() => {
-    return () => {
-      if (!interval) return;
-      clearInterval(interval);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    return () => destroyTimeUpdater();
   }, []);
 
   return (
     <>
-      <h1>{currentTime}</h1>
+      <h1>
+        {!!interval.current ? "y" : "n"} {shouldUpdateTime ? "y" : "n"}
+        {currentTime}
+      </h1>
       <YouTube
         videoId={board.resourceId}
         opts={opts}
-        onReady={onPlayerReady}
+        onStateChange={(e) => {
+          const state = normalizeState(e.data);
+          console.log("state", state);
+          if (state === "playing") {
+            destroyTimeUpdater();
+            createTimeUpdater(e);
+          } else if (state === "paused") {
+            destroyTimeUpdater()
+          }
+        }}
+        onReady={(e) => {
+          setShouldUpdateTime(true);
+        }}
       />
     </>
   );
