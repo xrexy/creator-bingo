a99d204f0469b145790842344ba57fb80b3ef614
xrexy <rexydoesimportantthings@gmail.com>
2023-11-24 11:32:37 +0200

    fix invalid dom property

--- src/app/settings/(tab)/profile/_components/YouTubeChannelForm.tsx
+++ src/app/settings/(tab)/profile/_components/YouTubeChannelForm.tsx
@@ -61,8 +61,8 @@ function DeleteCreatorForm({
           <path
             fill="none"
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M3.14 7.13L1.27 9a2.65 2.65 0 0 0 0 3.74h0a2.65 2.65 0 0 0 3.74 0l1.11-1.11M9 9.5h1.86a2.64 2.64 0 0 0 2.64-2.64h0a2.64 2.64 0 0 0-2.64-2.64H8.22M7 .5l-.5 2m-6 1l2 1m.5-4l1 2"
           />
         </svg>
