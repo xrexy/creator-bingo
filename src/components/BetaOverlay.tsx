dcaef6ac93da42e45e6acd21b933dead0ede5db0
xrexy <rexydoesimportantthings@gmail.com>
2023-11-24 12:06:56 +0200

    persist beta notification

--- src/components/BetaOverlay.tsx
+++ src/components/BetaOverlay.tsx
@@ -1,22 +1,33 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export type BetaOverlayProps = {};
 
 export function BetaOverlay({}: BetaOverlayProps) {
-  const [hidden, setHidden] = useState(false);
+  const [hidden, setHidden] = useState(true);
+
+  useEffect(() => {
+    const hidden = localStorage.getItem("beta-overlay-hidden");
+    setHidden(hidden === "true");
+  }, []);
 
   if (hidden) return null;
 
   return (
     <div className="absolute z-10 p-2 bg-black border-2 rounded-md border-gray-200/10 bottom-4 right-4">
-      <p>This is a <span className="font-semibold text-sky-400">Beta</span> build.</p>
+      <p>
+        This is a <span className="font-semibold text-sky-400">Beta</span>{" "}
+        build.
+      </p>
       <p className="text-sm ">Everything is subject to change.</p>
 
       <button
         className="absolute top-2 right-2"
-        onClick={() => setHidden(true)}
+        onClick={() => {
+          setHidden(true);
+          localStorage.setItem("beta-overlay-hidden", "true");
+        }}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
